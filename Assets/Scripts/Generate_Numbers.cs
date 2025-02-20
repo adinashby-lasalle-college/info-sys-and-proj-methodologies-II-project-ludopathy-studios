f2be48aa673e5a67b5df987ffdf9f3cec705e496
Eggnrice <johanbello2002@hotmail.com>
2025-02-20 14:53:47 -0500

    GridManager and GenerateNumbers Refactoring

--- Assets/Scripts/Generate_Numbers.cs
+++ Assets/Scripts/Generate_Numbers.cs
@@ -5,44 +5,51 @@ using TMPro;
 using UnityEngine.UI;
 using System.Linq;
 
-namespace Andres_Scene_Scripts{
+namespace Andres_Scene_Scripts
+{
     public class Generate_Numbers : MonoBehaviour
     {
-        public TMP_Text[] TxtBox; 
+        public TMP_Text[] TxtBox;
         public List<int> Numbers = new List<int>();
-        public Button[] PlBtns;
+        public Button[] TableBtns;
         public GridManager gridManager;
         public int[] MarkedSpace;
         public GameObject[] BingoTxt;
-        public static int[,] GenerateBingoCard;
 
-        // Start is called before the first frame update
+        public static Generate_Numbers instance;
+
+        void Awake()
+        {
+            if (instance != null && instance != this)
+            {
+                instance = this;
+            }
+        }
         private void Start()
         {
             PlayerSetup();
+            gridManager = GridManager.instance;
         }
 
         void PlayerSetup()
         {
             int[,] Card = BingoCardGenerator.GenerateBingoCard();
 
-            int col= 0;
+            int col = 0;
             int row = 0;
 
             // Assigning number to button text on the grid
             for (int j = 0; j < TxtBox.Length; j++)
             {
-                if(j % 5 == 0 && j != 0)
+                if (j % 5 == 0 && j != 0)
                 {
                     row++;
                     col = 0;
                 }
-                TxtBox[j].text = Card[row,col].ToString();
-                Numbers.Add(Card[row,col]);
+                TxtBox[j].text = Card[row, col].ToString();
+                Numbers.Add(Card[row, col]);
                 col++;
             }
-
-            gridManager = FindObjectOfType<GridManager>();
 
             //Set Marked Spaces value to zero
             for (int i = 0; i < MarkedSpace.Length; i++)
@@ -53,42 +60,8 @@ namespace Andres_Scene_Scripts{
 
         public void SetBtnUninteractable(int Number)
         {
-            PlBtns[Number].interactable = false;
+            TableBtns[Number].interactable = false;
         }
 
-        public void WinCondition()
-        {
-            //Hotizontal
-            int s1 = MarkedSpace[0] + MarkedSpace[1] + MarkedSpace[2] + MarkedSpace[3] + MarkedSpace[4];
-            int s2 = MarkedSpace[5] + MarkedSpace[6] + MarkedSpace[7] + MarkedSpace[8] + MarkedSpace[9];
-            int s3 = MarkedSpace[10] + MarkedSpace[11] + MarkedSpace[12] + MarkedSpace[13] + MarkedSpace[14];
-            int s4 = MarkedSpace[15] + MarkedSpace[16] + MarkedSpace[17] + MarkedSpace[18] + MarkedSpace[19];
-            int s5 = MarkedSpace[20] + MarkedSpace[21] + MarkedSpace[22] + MarkedSpace[23] + MarkedSpace[24];
-            
-            //Vertical
-            int s6 = MarkedSpace[0] + MarkedSpace[5] + MarkedSpace[10] + MarkedSpace[15] + MarkedSpace[20];
-            int s7 = MarkedSpace[1] + MarkedSpace[6] + MarkedSpace[11] + MarkedSpace[16] + MarkedSpace[21];
-            int s8 = MarkedSpace[2] + MarkedSpace[7] + MarkedSpace[12] + MarkedSpace[17] + MarkedSpace[22];
-            int s9 = MarkedSpace[3] + MarkedSpace[8] + MarkedSpace[13] + MarkedSpace[18] + MarkedSpace[23];
-            int s10 = MarkedSpace[4] + MarkedSpace[9] + MarkedSpace[14] + MarkedSpace[19] + MarkedSpace[24];
-
-            //Diagonal
-            int s11 = MarkedSpace[0] + MarkedSpace[6] + MarkedSpace[12] + MarkedSpace[18] + MarkedSpace[24];
-            int s12 = MarkedSpace[4] + MarkedSpace[8] + MarkedSpace[12] + MarkedSpace[16] + MarkedSpace[20];   
-
-            var solution = new int[] { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12};
-
-            int SumOfMarkedSpace = -1;
-
-            foreach(var sol in solution)
-            {
-                if(sol == 5)
-                {
-                    SumOfMarkedSpace++;
-                    Debug.Log(gameObject.name + " " + SumOfMarkedSpace);
-                    BingoTxt[SumOfMarkedSpace].SetActive(true);
-                }
-            }
-        }
     }
 }
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -3,93 +3,45 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Andres_Scene_Scripts{
-    
-public class GridManager : MonoBehaviour
+namespace Andres_Scene_Scripts
 {
-    public int Turn;
-    public int TurnCount;
 
-    public GameObject[] TurnIcon;
-
-    public GameObject[] Player;
-
-    public GameObject[] CanvasBlock;
-
-    public string SelectedNum1;
-    public string SelectedNum2;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class GridManager : MonoBehaviour
     {
-        GameSetup();
-    }
 
-    void GameSetup()
-    {
-        Turn = 0;
-        TurnCount = 0;
+        public static GridManager instance;
+        Generate_Numbers Table;
+        public GameObject[] BallDrawnGO;
+        public GameObject[] CanvasBlock;
 
-        TurnIcon = GameObject.FindGameObjectsWithTag("Holder");
-
-        TurnIcon[0].SetActive(true);
-        TurnIcon[1].SetActive(false);
-
-        CanvasBlock[0].SetActive(false);
-        CanvasBlock[1].SetActive(true);
-    }
-
-    public void GridButtons(int Number)
-    {
-        var Pl1 = Player[0].GetComponentInChildren<Generate_Numbers>();
-        // var Pl2 = Player[1].GetComponentInChildren<Generate_Numbers>();
-
-        //Change Player on Button Click - 0 - player 1, 1 - player 2 
-        if (Turn == 0)
+        void Awake()
         {
-            Turn = 1;
-            TurnIcon[0].SetActive(false);
-            //TurnIcon[1].SetActive(true);
-
-            CanvasBlock[0].SetActive(true);
-            //CanvasBlock[1].SetActive(false);
-
-            Pl1.MarkedSpace[Number] = 1;
-            SelectedNum1 = Pl1.TxtBox[Number].text;
-                    
-
-            Pl1.WinCondition();
-        }
-        else
-        {
-            Turn = 0;
-
-            TurnIcon[0].SetActive(true);
-            //TurnIcon[1].SetActive(false);
-
-            CanvasBlock[0].SetActive(false);
-            //CanvasBlock[1].SetActive(true);
-
-            // Pl2.MarkedSpace[Number] = 1;
-            // SelectedNum2 = Pl2.TxtBox[Number].text;
-
-            int TboxLength = Pl1.TxtBox.Length;
-            for (int i = 0; i < TboxLength; i++)
+            if (instance != null && instance != this)
             {
-                var Num = Pl1.TxtBox[i].text;
-                if (SelectedNum2 == Num)
-                {
-                    Pl1.PlBtns[i].interactable = false;
-
-                    Pl1.MarkedSpace[i] = 1;
-                }
+                Destroy(gameObject);
             }
+            instance = this;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            GameSetup();
+            Table = Generate_Numbers.instance;
+            BallDrawnGO = GameObject.FindGameObjectsWithTag("Holder");
+        }
 
+        void GameSetup()
+        {
+            Debug.Log("Game Start");
+        }
+
+        public void GridButtons(int Number)
+        {
+            Table.MarkedSpace[Number] = 1;
         }
     }
-}
 }
